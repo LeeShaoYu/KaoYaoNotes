08b404b6c12fbed103ca869129a71c2211de447c
byteoo <1663423581@qq.com>
2020-10-19 13:42:08 +0800

    frist commit

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'KaoYanNotes',
+  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  base: '/仓库名称',
+  publicPath: '/仓库名称/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  // more config: https://d.umijs.org/config
+});
